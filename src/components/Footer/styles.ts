cae8305ff1bd565b1503d91429e5a9bf14342639
Noemia <noemia.aso@brseikyo.com.br>
2021-01-22 16:45:40 -0300

    Página Mercado Livre
    Recriação da página

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { getAutomaticTypeDirectiveNames } from "typescript";
 
 export const Container = styled.div`
   width: 100%;
@@ -12,3 +13,11 @@ export const Container = styled.div`
 
   margin-top: 30px;
 `;
+
+export const Wrapper = styled.div`
+max-width: 1220px;
+margin: 0 auto;
+padding: 0 16px;
+`;
+ 
+
