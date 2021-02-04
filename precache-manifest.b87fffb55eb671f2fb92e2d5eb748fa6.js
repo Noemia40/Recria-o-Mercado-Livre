c93e8580d1aa9cca6cc26029df2f207f06e4d5b9
Noemia Taemi Aso Fukumoto <77854291+Noemia40@users.noreply.github.com>
2021-02-04 15:49:30 -0300

    Updates

---
+++ precache-manifest.b87fffb55eb671f2fb92e2d5eb748fa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bba90223f85760df8b3d8260b5448ce1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff8428f0e4709fe05b57",
+    "url": "/static/js/2.9393c385.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.9393c385.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef889cbc4eca0958f0c1",
+    "url": "/static/js/main.ebf6d510.chunk.js"
+  },
+  {
+    "revision": "140ac787906f27ef248e",
+    "url": "/static/js/runtime-main.ff723d3d.js"
+  },
+  {
+    "revision": "7df03694fbb128e2b06e30c2f883f42e",
+    "url": "/static/media/tshirt.7df03694.png"
+  }
+]);
